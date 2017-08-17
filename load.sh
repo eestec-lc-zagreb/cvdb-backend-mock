e2f7cc77645a0db806a811bf6aaa4bab027b5f62
Alen Hrga <alen.hrga@ingemark.com>
2017-08-17 13:43:13 +0200

    added settings to load.sh

--- load.sh
+++ load.sh
@@ -3,6 +3,20 @@
 SDIR=$(cd `dirname $0` && pwd)
 WSRV=http://localhost:61950
 
+# load mappings
 curl -X DELETE $WSRV/__admin/mappings
 find $SDIR -name \*.json -exec curl $WSRV/__admin/mappings --data-binary @{} \;
 
+# load settings
+SETTINGS="{ \
+              \"delayDistribution\": { \
+                      \"type\": \"lognormal\", \
+                      \"median\": 90, \
+                      \"sigma\": 0.1 \
+              } \
+          }"
+
+
+SETTINGS_URL="$WSRV/__admin/settings"
+
+curl -X POST $SETTINGS_URL --data-binary "$SETTINGS";
